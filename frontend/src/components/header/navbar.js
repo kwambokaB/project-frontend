e52766ebbc5620bc65b9d00fac81e5d157a372d8
kwambokaB <paula.paulatek@gmail.com>
2020-01-24 14:05:18 +0300

    initial header structure

--- frontend/src/components/header/navbar.js
+++ frontend/src/components/header/navbar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 
 function Navbar(){
     return(
         <div className = 'nav-bar'>
+        <nav>
+            <ul>
+                <li><a href='#'>About us</a></li>
+                <li><a href='#'>Become a councellor</a></li>
+                <li><a href='#'>blog</a></li>
+                <li><a href='#'></a>contact</li>
+                <li><a href='#'>login</a></li>
+            </ul>
+        </nav>
 
         </div>
     );
