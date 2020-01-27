7d4c32237aec50567fb12a3dcf74a7ec4d0ed61a
kwambokaB <paula.paulatek@gmail.com>
2020-01-27 16:26:07 +0300

    very badly done landing page- still working on it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from './components/header/header';
+import Landing from './components/landing-page/landingpage';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App container">
+     <div className="container">
       <Header />
+      </div>
+      <div className="container">
+        <Landing />
+      </div>
     </div>
   );
 }
--- frontend/src/components/landing-page/landingpage.js
+++ frontend/src/components/landing-page/landingpage.js
@@ -1,14 +1,47 @@
-import  React, { Component } from 'react'
+import  React, { Component } from 'react';
+import './landing.css';
  
-class Landing extends Component {
-  render {
-      return(
-        <div className="container">
+function Landing(){
+  return(
+    <div className="content">
+    <div className="section-1">
+    <div id="description" className="card">
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+    
+    <div id="signup" className="card">
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+    <div id="button" className="button">
+      <button id="talk">
+        Talk to a therapist
+      </button>
+      <button id="community">
+        Engange in our community
+        </button>
+    </div>
 
-        </div>
+    </div>
+    <div className="section-2">
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat commodo sed egestas egestas fringilla phasellus faucibus. Scelerisque mauris pellentesque pulvinar pellentesque. Nibh sit amet commodo nulla facilisi nullam. Non odio euismod lacinia at quis. A diam maecenas sed enim ut sem viverra aliquet. Et leo duis ut diam quam nulla porttitor massa id. Sem integer vitae justo eget. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Dui nunc mattis enim ut tellus elementum sagittis vitae. Faucibus in ornare quam viverra orci. Sagittis nisl rhoncus mattis rhoncus.
+    </p>
 
-      );
-  };
+    </div>
+    <div className="section-3">
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat commodo sed egestas egestas fringilla phasellus faucibus. Scelerisque mauris pellentesque pulvinar pellentesque. Nibh sit amet commodo nulla facilisi nullam. Non odio euismod lacinia at quis. A diam maecenas sed enim ut sem viverra aliquet. Et leo duis ut diam quam nulla porttitor massa id. Sem integer vitae justo eget. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Dui nunc mattis enim ut tellus elementum sagittis vitae. Faucibus in ornare quam viverra orci. Sagittis nisl rhoncus mattis rhoncus.
+    </p>
+
+    </div>
+       
+    </div>
+
+  );
 };
 
 export default Landing;
